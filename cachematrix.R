1de0bed97e54f75dd2d21090c982fda7b3daff2e
dersuid <rgusthavom@hotmail.com>
2015-11-19 20:08:29 -0600

    Final R Programming Assignment 2
    Has two functions in order to run in a cache environment, so we can
    improve the memory in huge computations.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## the first function calls a squared matrix, store and solve it (get the inverse) for the first time
+## the second function, calls the first function in order to confirm if that matrix
+## has already its inverse, if not, compute the inverse for x matrix
+
+## this function has for functions within it. First save the x matrix, get the inverse of it, 
+## keep the solution and store in in cache, and then returns a list containing the 4   functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
+## this function, validates if the inverse of x has been already computed, if not, then
+## computes the inverse for the first time, if it is already estimated, then calls the makeCacheMatrix
+## and take the result of solve (x) for returning
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
